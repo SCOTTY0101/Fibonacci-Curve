2737b6e807c2df185294fc3c1e4e70fb29c465bf
Alan George <45034425+SCOTTY0101@users.noreply.github.com>
2020-10-17 06:00:41 +1000

    Update index.js

--- index.js
+++ index.js
@@ -2,5 +2,8 @@
 
 let a = 0, b = 1, c = 0;
 
-for (let i = 2; i < Infinity; i++) { a = b; b = c; c = a + b; i = (c = a + b); 
-console.log(i);};
+for (let i = 2; i < Infinity; i++) { 
+  a = b; b = c; c = a + b; i = (c = a + b); 
+    console.log(i);};
+
+
