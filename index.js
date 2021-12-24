4e46fd0fb152257aabc89ea1b069c2edf6c46e8d
Alan George <45034425+SCOTTY0101@users.noreply.github.com>
2021-12-24 12:44:43 +1000

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 //Fibonacci curve. Loops to infinity. JavaScript.
 
-let a = 0, b = 1, c = 0;
+//let a = 0, b = 1, c = 0;
+let a = 0;
+let b = 1;
+let c = 0;
 
 for (let i = 2; i < Infinity; i++) { 
   a = b; 
