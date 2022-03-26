5457afc40df2090f1956493cbaf4a12b2ca39f95
Alan George <45034425+SCOTTY0101@users.noreply.github.com>
2022-03-26 18:32:06 +1000

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
 //Fibonacci curve. Loops to infinity. JavaScript.
 
-//let a = 0, b = 1, c = 0;
+/*
+*Declare and assign the variable values
+*/
+
 let a = 0;
 let b = 1;
 let c = 0;
+
+//Looping through the fibonacci curve so the sum is equal to the sum of the preceding two numbers.
+//Use less than infinity in the for loop or you'll end up like Buzz Lightyyear and go to 'infinity and beyond'!
 
 for (let i = 2; i < Infinity; i++) { 
   a = b; 
   b = c; 
   c = a + b; 
   i = (c = a + b); 
-    console.log(i);
+    console.log(i); //Output the valves to the console. Use webpage inspection to view on your browser.
 };
 
 
